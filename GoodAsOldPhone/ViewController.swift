e8e1d0f09467f3f52468b1d131f36527885a838b
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2016-01-02 12:29:02 +0700

    Initial Commit

---
+++ GoodAsOldPhone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodAsOldPhone
+//
+//  Created by Wassapon Watanakeesuntorn on 1/2/2016.
+//  Copyright © 2016 Wassapon Watanakeesuntorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
